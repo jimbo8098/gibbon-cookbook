769518b95500190aef93c251f5f537109fe00de3
Jim Speir <jimbo80982@gmail.com>
2020-04-09 23:05:35 +0100

    apache2_helper now adds modules using apache.modules in the environment file.

--- helpers/apache2_helper/recipes/default.rb
+++ helpers/apache2_helper/recipes/default.rb
@@ -7,35 +7,6 @@ service 'apache2' do
   action [:start, :enable]
 end
 
-apache2_module 'mod_rewrite'
-
-app_dir = '/var/www/gibbon'
-
-directory app_dir do
-  recursive true
+node['apache']['modules'].each do |mod|
+  apache2_module mod
 end
-
-file "#{app_dir}/index.html" do
-  content 'Hello World'
-  extend  Apache2::Cookbook::Helpers
-  owner   lazy { default_apache_user }
-  group   lazy { default_apache_group }
-end
-#
-#template 'basic_site' do
-#  extend  Apache2::Cookbook::Helpers
-#  source 'basic_site.conf.erb'
-#  path "#{apache_dir}/sites-available/basic_site.conf"
-#  variables(
-#    server_name: '127.0.0.1',
-#    document_root: app_dir,
-#    log_dir: lazy { default_log_dir },
-#    site_name: 'basic_site'
-#  )
-#end
-#
-#apache2_site 'basic_site'
-#
-#apache2_site '000-default' do
-#  action :disable
-#end
