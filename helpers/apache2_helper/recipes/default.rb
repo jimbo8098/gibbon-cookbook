411c5a82ad0dae3c8ac09c6f83c7ceca047f85f7
Jim Speir <jimbo80982@gmail.com>
2020-04-14 17:19:36 +0100

    Apache site is now generated, not using HTTPS yet

--- helpers/apache2_helper/recipes/default.rb
+++ helpers/apache2_helper/recipes/default.rb
@@ -10,3 +10,20 @@ end
 node['apache']['modules'].each do |mod|
   apache2_module mod
 end
+
+puts node['gibbon'].inspect
+template node['gibbon']['site_name'] do
+  extend  Apache2::Cookbook::Helpers
+  source "site.conf.erb"
+  path "#{apache_dir}/sites-available/#{node['gibbon']['site_name']}.conf"
+  variables(
+    server_name: node['gibbon']['site_name'],
+    document_root: node['gibbon']['gibbon_dir'],
+    log_dir: lazy { default_log_dir },
+    site_name: node['gibbon']['site_name'],
+    port: node['gibbon']['port'],
+    ip: node['gibbon']['ip']
+  )
+end
+
+apache2_site node['gibbon']['site_name']
